7f16e739195fc2dd2ebba2b9df3b7a8cbb14459b
millires <eml_box@hotmail.com>
2025-04-17 10:20:23 +0100

    added-game-pages

---
+++ app/ImageList.jsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+import { Text, View, Image, Pressable, FlatList } from "react-native";
+
+import questionMark from '../assets/images/question-mark.png'
+
+function timeout(delay) {
+    return new Promise( res => setTimeout(res, delay) );
+}
+const ImageList = ({imagesData, imgStyle}) => {
+
+    console.log('top .... ', imagesData)
+
+    const [selectedImg, setSelectedImg] = useState([])
+    const [matchedImg, setMatchedImg] = useState([])
+    const [imgSide, setImgSide] = useState(imagesData.map(() => false ));
+
+    const [isGameOver, setIsGameOver] = useState(false)
+    const [flag, setFlag] = useState(false);
+
+    const toggleFlag = () => {
+        console.log('toggle flag ....')
+        setFlag(previousState => !previousState); 
+    };
+
+    const turnImg = (name, index) => {
+        const selected = selectedImg.find(item => item.index === index)
+        console.log('turn img .... ', imgSide[index], index, selected)
+        //if (imgSide[index]){
+            if(!selected && selectedImg.length < 2)
+                setSelectedImg( prev => [...prev, {name, index}])
+            else if (selectedImg.length === 2)
+                setSelectedImg([{name, index}])
+        //} //else
+        //    imgSide[index] = true
+    }
+        
+    //console.log('in imageList .... ', Data)
+      
+       
+    useEffect (() => {
+        console.log('useEffect .... image clicked')
+        if (selectedImg.length === 2 && selectedImg[0].name === selectedImg[1].name)
+            setMatchedImg((prev) => [...prev, ...selectedImg])
+        else if (selectedImg.length === 2) {
+            imgSide[selectedImg[0].index] = false;
+            imgSide[selectedImg[1].index] = false
+            //setImgSide(...[imgSide])
+            setTimeout(() => {
+                console.log('waitning .... ')
+                toggleFlag()
+            }, 500);
+        }
+    }, [selectedImg])
+
+    useEffect (() => {
+        if (imagesData.length && imagesData.length === matchedImg.length)
+            setIsGameOver(true)
+    }, [matchedImg])
+
+
+  console.log('imageSide .... ', imgSide)
+  console.log('selectImg, matches, gameover... ', selectedImg, matchedImg, isGameOver)
+
+
+    return (
+        <>
+        <Text>{isGameOver ? "You WIN, all cards matched" : matchedImg.length > 0 ? `You have mactched ${matchedImg.length} cards` : "Press on image to play"} </Text>
+        
+        <FlatList 
+            data = {imagesData}
+            keyExtractor={(_, index) => index}
+            contentContainerStyle={{
+                flexDirection: "row",
+                numColumns: 4,
+                flexWrap: "wrap",
+                justifyContent: "center",
+                width: '100%',
+                maxWidth: 400,
+                gap: 5,
+                marginTop: 20,    
+            }}
+            showsVerticalScrollIndicator={false}
+            ListEmptyComponent={<Text>No Game Available</Text>}
+            renderItem={({item, index}) => (
+                <View>
+                    <Pressable onPress={() =>{
+                        const imgName = item.uri.split('\\').pop().split('/').pop().split('.').shift();
+                        console.log('pressed on ..... ', index, imgName);
+                        imgSide[index] ? true : imgSide[index] = true
+                        turnImg(imgName, index)
+                        toggleFlag()
+                    }}>
+                        <Image source={ imgSide[index] ? item : questionMark } style={imgStyle}/>
+                    </Pressable>
+                </View>
+            )}
+        /></>
+    )
+}
+
+export default ImageList
---
+++ constants/Images.jsx
@@ -0,0 +1,27 @@
+import one from '@/assets/images/number-1.png'
+import two from '@/assets/images/number-2.png'
+import three from '@/assets/images/number-3.png'
+import four from '@/assets/images/number-4.png'
+import five from '@/assets/images/number-5.png'
+import six from '@/assets/images/number-6.png'
+import seven from '@/assets/images/number-7.png'
+import eight from '@/assets/images/number-8.png'
+import nine from '@/assets/images/number-9.png'
+import ten from '@/assets/images/number-10.png'
+import eleven from '@/assets/images/number-11.png'
+import twelve from '@/assets/images/number-12.png'
+import watermelon from '@/assets/images/watermelon.png'
+import strawberry from '@/assets/images/strawberry.png'
+import strawberry_2 from '@/assets/images/strawberry_2.png'
+import orange from '@/assets/images/orange.png'
+import grapes from '@/assets/images/grapes.png'
+import fruit from '@/assets/images/fruit.png'
+import fruit_salad from '@/assets/images/fruit-salad.png'
+import apple from '@/assets/images/apple.png'
+
+const numbersImages = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve,
+    watermelon, strawberry, strawberry_2, orange, grapes, fruit, fruit_salad, apple
+]
+
+export default numbersImages
+//export {one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve}
