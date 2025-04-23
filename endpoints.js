a6b4782a6c9a4032052343efb4f0e0b0928c2038
Isabella <isabella.hunter2001@gmail.com>
2025-04-23 16:02:25 +0100

    Home page (#26)
    * home page messages showing with backend data
    
    * friend game scores rendering on home page
    
    * scores for logged in users rendering on home page
    
    * friend game name now rendering
    
    * styling fixes

--- endpoints.js
+++ endpoints.js
@@ -13,7 +13,21 @@ export function getUsers() {
 }
 export function getUserByUsername(username) {
   return apiClient.get(`/users/${username}`).then(({ data }) => {
-    console.log(data.user);
     return data.user;
   });
+}
+export function getMessagesByUsername(username) {
+  return apiClient.get(`/users/${username}/messages`).then(({data}) => {
+    return data.messages
+  })
+}
+export function getScores(){
+  return apiClient.get(`/scoreboard`).then(({data}) => {
+    return data.scoreboard
+  })
+}
+export function getGames(){
+  return apiClient.get(`/games`).then(({data}) => {
+    return data.games
+  })
 }
